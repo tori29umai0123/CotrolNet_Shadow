8a6e9a99d9e0477bca8b30dfbd863b5c8925de60
tori29umai <72191117+tori29umai0123@users.noreply.github.com>
2023-06-03 02:25:18 +0900

    v1.2

--- Assets/Scripts/Camera_Capture.cs
+++ Assets/Scripts/Camera_Capture.cs
@@ -1,29 +1,31 @@
 using UnityEngine;
 using System.IO;
 using System.Collections;
-using System;
 
 public class Camera_Capture : MonoBehaviour
 {
     public GameObject ModelCamera;
     public GameObject LightCamera;
     public GameObject DirectionalLight;
+    public GameObject model;
+    public int captureW;
+    public int captureH;
 
+    public bool IsLoading;
     public Vector3 Pos;
-    public ModelLoader ModelLoader;
     public string outpath;
     public string modelpath;
     public int count;
     public int max_count;
     public int model_count;
-    public int load_count;
-    public bool load;
+
+    private bool isCapturing; // 撮影中かどうかを管理するフラグ
 
     private void Start()
     {
-        count = -1;
-        model_count = Directory.GetFiles(modelpath, "*", SearchOption.TopDirectoryOnly).Length;
-        StartCoroutine(RandomCapture());
+        count = 0;
+        model_count = Directory.GetFiles(modelpath, "*", SearchOption.TopDirectoryOnly).Length/2;
+        isCapturing = false; // 撮影フラグを初期化
 
         string targetFolderPath = Path.Combine(outpath, "target");
         if (!Directory.Exists(targetFolderPath))
@@ -36,74 +38,101 @@ public class Camera_Capture : MonoBehaviour
         {
             Directory.CreateDirectory(lightFolderPath);
         }
+        StartCapture();
     }
 
-    private void ModelLoad(string model_name)
+    private void StartCapture()
     {
-        ModelLoader.FileLoad(modelpath + "/" + model_name + ".obj");
-        Debug.Log("Start Heavy Method: " + System.Threading.Thread.CurrentThread.ManagedThreadId);
-        System.Threading.Thread.Sleep(1000);
-        Debug.Log("End Heavy Method: " + System.Threading.Thread.CurrentThread.ManagedThreadId);
+        if (!isCapturing)
+        {
+            StartCoroutine(RandomCapture());
+        }
+    }
+
+    bool ModelLoad(string model_name)
+    {
+        GameObject modelPrefab = Resources.Load<GameObject>(model_name);
+
+        model = Instantiate(modelPrefab);
+
+        //modelの座標がズレる時の修正用。位置がおかしくなったらコメントアウトしてみて
+        foreach (Transform childTransform in model.transform)
+        {
+            if (childTransform.gameObject.name != "default.default")
+            {
+                var child = childTransform.gameObject;
+                Pos = child.GetComponent<Renderer>().bounds.center;
+                float x = -Pos.x;
+                float y = -Pos.y;
+                float z = -Pos.z;
+                model.transform.Translate(x, y, z);
+            }
+            if (model.transform.childCount == 1)
+            {
+                var child = childTransform.gameObject;
+                Pos = child.GetComponent<Renderer>().bounds.center;
+                float x = -Pos.x;
+                float y = -Pos.y;
+                float z = -Pos.z;
+                model.transform.Translate(x, y, z);
+            }
+        }
+        IsLoading = true ;
+        return IsLoading;
     }
 
     private IEnumerator RandomCapture()
     {
-        while (true)
+        isCapturing = true; // 撮影フラグを設定
+
+        while (count < max_count)
         {
-            count += 1;
+            count++; // count の値をインクリメント
+
             int model_rnd = UnityEngine.Random.Range(0, model_count);
-            float rot_rnd = UnityEngine.Random.Range(-360, 360);
             string model_name = model_rnd.ToString("0000");
-            ModelLoad(model_name);
+
+            yield return new WaitUntil(() => ModelLoad(model_name) == true); // モデルのロードが完了するまで待機
+                                                                              // モデルのロードが完了した後に行う処理をここに記述する
+                                                                              // ランダム光源及びランダムカメラ配置
+            float rot_rnd = UnityEngine.Random.Range(-360, 360);
             ModelCamera.transform.RotateAround(Pos, Vector3.up, rot_rnd);
             DirectionalLight.transform.eulerAngles = new Vector3(rot_rnd, 90, 90);
-            yield return new WaitForSeconds(1);
 
-            if (count > 0)
-            {
-                if (load)
-                {
-                    count = load_count;
-                    load = false;
-                }
-                CameraSavePng_Model(count.ToString("00000"));
-                CameraSavePng_Light(count.ToString("00000"));
-            }
 
-            if (count == max_count + 1)
+            // ここで撮影を行う
+            CameraSavePng_Model(count.ToString("00000"));
+            CameraSavePng_Light(count.ToString("00000"));
+
+            // モデルの撮影が完了したらGameObjectを破棄する
+            Destroy(model);
+
+            IsLoading = false;
+            if (count >= max_count)
             {
-                break;
+                UnityEditor.EditorApplication.isPlaying = false;
             }
         }
-    }
 
-    private void Update()
-    {
-        if (count == max_count + 1)
-        {
-            UnityEditor.EditorApplication.isPlaying = false;
-        }
+        isCapturing = false; // 撮影フラグを解除
     }
 
     private void CameraSavePng_Model(string count)
     {
-        int captureSizeX = 512;
-        int captureSizeY = 512;
-
         Camera camera = ModelCamera.GetComponent<Camera>();
 
-        var rt = new RenderTexture(captureSizeX, captureSizeY, 24);
-        var prev = camera.targetTexture;
+        RenderTexture rt = new RenderTexture(captureW, captureH, 24);
+        RenderTexture prev = camera.targetTexture;
         camera.targetTexture = rt;
         camera.Render();
         camera.targetTexture = prev;
         RenderTexture.active = rt;
 
-        var capture = new Texture2D(captureSizeX, captureSizeY, TextureFormat.ARGB32, false);
+        Texture2D capture = new Texture2D(captureW, captureH, TextureFormat.ARGB32, false);
         capture.ReadPixels(new Rect(0, 0, capture.width, capture.height), 0, 0);
         capture.Apply();
-        var bytes = capture.EncodeToPNG();
-        var path = Path.Combine(outpath, "target", count + ".png");
+        byte[] bytes = capture.EncodeToPNG();
+        string path = Path.Combine(outpath, "target", count + ".png");
         if (string.IsNullOrEmpty(path))
         {
             return;
@@ -117,23 +146,20 @@ public class Camera_Capture : MonoBehaviour
 
     private void CameraSavePng_Light(string count)
     {
-        int captureSizeX = 512;
-        int captureSizeY = 512;
-
         Camera camera = LightCamera.GetComponent<Camera>();
 
-        var rt = new RenderTexture(captureSizeX, captureSizeY, 24);
-        var prev = camera.targetTexture;
+        RenderTexture rt = new RenderTexture(captureW, captureH, 24);
+        RenderTexture prev = camera.targetTexture;
         camera.targetTexture = rt;
         camera.Render();
         camera.targetTexture = prev;
         RenderTexture.active = rt;
 
-        var capture = new Texture2D(captureSizeX, captureSizeY, TextureFormat.ARGB32, false);
+        Texture2D capture = new Texture2D(captureW, captureH, TextureFormat.ARGB32, false);
         capture.ReadPixels(new Rect(0, 0, capture.width, capture.height), 0, 0);
         capture.Apply();
-        var bytes = capture.EncodeToPNG();
-        var path = Path.Combine(outpath, "light", count + ".png");
+        byte[] bytes = capture.EncodeToPNG();
+        string path = Path.Combine(outpath, "light", count + ".png");
         if (string.IsNullOrEmpty(path))
         {
             return;
